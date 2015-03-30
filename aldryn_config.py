6f139077355ee5249ccc45a485f1e02ae673bf6d
Martin Koistinen <mkoistinen@gmail.com>
2015-03-30 13:53:01 +0300

    Clean up deployment

--- aldryn_config.py
+++
@@ -1,9 +0,0 @@
-# from aldryn_client import forms
-
-
-# class Form(forms.BaseForm):
-#     property = forms.CharField('description', required=False)
-
-#     def to_settings(self, data, settings):
-#         settings['SOME_SETTING'] = data.get('property', '')
-#         return settings
